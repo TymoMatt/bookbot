829a29028791a2593c11c90554eb64c2a3a626fc
TymoMatt <tymoshenko.matt@gmail.com>
2025-10-30 14:41:24 -0400

    Add system args

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_num_words
 from stats import get_char_count_dict
 from stats import get_sorted_list
@@ -27,7 +29,11 @@ def print_char_count(dict):
 
 def main():
 	try:
-		filepath = "books/frankenstein.txt"
+		if len(sys.argv) != 2:
+			print("Usage: python3 main.py <path_to_book>")
+			sys.exit(1)
+
+		filepath = sys.argv[1]
 		print_title(filepath)
 
 		file = get_book_text(filepath)
